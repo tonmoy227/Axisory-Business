750c3ddb21e255b0ec6f45e357b1cfc4245e523f
tonmoy227 <145573783+tonmoy227@users.noreply.github.com>
2026-02-01 10:22:17 +0600

    100%
    100%

--- assets/js/script.js
+++ assets/js/script.js
@@ -613,6 +613,9 @@ Last change:    00/00/00
 		stagger: -.2,
 	})
 
+
+
+
 	gsap.utils.toArray(' .top_view').forEach((el, index) => { 
 		let tlcta = gsap.timeline({
 			scrollTrigger: {
@@ -620,6 +623,23 @@ Last change:    00/00/00
 				scrub: 4,
 				start: "top 100%",
 				end: "top 30%",
+				toggleActions: "play none none reverse",
+				markers: false
+			}
+		})
+
+		tlcta
+		.set(el, {transformOrigin: 'center center'})
+		.from(el, {  scale: 1, y: "300"})
+	});
+
+	gsap.utils.toArray(' .top_view8').forEach((el, index) => { 
+		let tlcta = gsap.timeline({
+			scrollTrigger: {
+				trigger: el,
+				scrub: 4,
+				start: "top 20%",
+				end: "top 0%",
 				toggleActions: "play none none reverse",
 				markers: false
 			}
@@ -849,8 +869,12 @@ Last change:    00/00/00
 		var swiper3 = new Swiper(".ax-tst-thumb-slider", {
 			speed: 500,
 			loop: true,
-			slideToClickedSlide: true,
+			autoplay: {
+				delay: 6000,
+			},
+			slideToClickedSlide: false,
 			centeredSlides: true,
+			allowTouchMove: false,
 			breakpoints: {
 				0: {
 					slidesPerView: 5,
@@ -877,14 +901,40 @@ Last change:    00/00/00
 			fadeEffect: {
 				crossFade: true
 			},
-			// autoplay: {
-			// 	delay: 6000,
-			// },
+			autoplay: {
+				delay: 6000,
+			},
 			thumbs: {
 				swiper: swiper3,
 			},
 
 		});
 	}
+
+
+	if (window.matchMedia("(min-width: 1200px)").matches) { 
+		var WHYchoose = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.ax-why-c2-content',
+				start: "top 70%",
+				end: "top 20%",
+				scrub: 1,
+				markers: false,
+			}
+
+		});
+		WHYchoose
+		.from( ".ax-wc2-item2 .item-item .hand-img1" , {   x: -300,   duration: 2, ease: "power2.out"})
+		.from( ".ax-wc2-item2 .item-item .hand-img2" , {   x: 300,   duration: 2, ease: "power2.out"},"< .5")
+		.from( ".ax-wc2-item2 .item-img-wrap li:nth-child(2)" , { opacity: 0,  x: 100, rotate: 15,   duration: 2, ease: "power2.out"},"< .5")
+		.from( ".ax-wc2-item2 .item-img-wrap li:nth-child(4)" , { opacity: 0,  x: 100, rotate: 15,   duration: 2, ease: "power2.out"},"< .5")
+		.from( ".ax-wc2-item2 .item-img-wrap li:nth-child(3)" , { opacity: 0,  x: -100, rotate: -15,   duration: 2, ease: "power2.out"},"< .5")
+		.from( ".ax-wc2-item2 .item-img-wrap li:nth-child(5)" , { opacity: 0,  x: -100, rotate: -15,   duration: 2, ease: "power2.out"},"< .5")
+
+	};
+
+
+
+
 
 })(jQuery);
